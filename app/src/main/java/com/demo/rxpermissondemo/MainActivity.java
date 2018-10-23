92e1b4ba39a5693d4f871369e4345309ddc95842
BINBINXIAO <460116602@qq.com>
2018-10-23 13:20:27 +0800

    Update MainActivity.java

--- app/src/main/java/com/demo/rxpermissondemo/MainActivity.java
+++ app/src/main/java/com/demo/rxpermissondemo/MainActivity.java
@@ -1,13 +1,108 @@
 package com.demo.rxpermissondemo;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.demo.rxpermissondemo.permission.RxPermissions;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RxPermissions rxPermissions;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        rxPermissions = new RxPermissions(this);
+        check();
+
     }
+
+
+    private void check() {
+        rxPermissions.request(Manifest.permission.ACCESS_COARSE_LOCATION).subscribe(new Observer<Boolean>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Boolean aBoolean) {
+                if (aBoolean) {
+                    checkLocation2();
+                } else {
+                    Toast.makeText(MainActivity.this, "沒有位置权限，无法使用此软件，请前往手机设置开启对应功能", Toast.LENGTH_SHORT).show();
+                    getAppDetailSettingIntent();
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+    }
+
+
+    private void checkLocation2() {
+        rxPermissions.request(Manifest.permission.ACCESS_FINE_LOCATION)
+                .subscribe(new Observer<Boolean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onNext(Boolean aBoolean) {
+                        if (aBoolean) {
+                            //
+                        } else {
+                            Toast.makeText(MainActivity.this, "沒有位置权限，无法使用此软件，请前往手机设置开启对应功能", Toast.LENGTH_SHORT).show();
+                            getAppDetailSettingIntent();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+
+                    @Override
+                    public void onComplete() {
+                    }
+                });
+
+    }
+
+    /**
+     * 需要进一步适配。
+     */
+    private void getAppDetailSettingIntent() {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (Build.VERSION.SDK_INT >= 9) {
+            intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+            intent.setData(Uri.fromParts("package", getPackageName(), null));
+        } else if (Build.VERSION.SDK_INT <= 8) {
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
+            intent.putExtra("com.android.settings.ApplicationPkgName", getPackageName());
+        }
+        startActivity(intent);
+    }
+
 }
